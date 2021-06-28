dc99eed94b56c9b084b65d064b265dc866aacdd5
jcam-ibm <79992788+jcam-ibm@users.noreply.github.com>
2021-06-28 14:06:58 -0500

    Update ibmdemos-handsonlab-removal-support.m

--- IBM-Demos/IBM-Demos-Runbooks/ibmdemos-handsonlab-removal-support.m
+++ IBM-Demos/IBM-Demos-Runbooks/ibmdemos-handsonlab-removal-support.m
@@ -1,1 +1,25 @@
+# IBM Demos hands-on lab support responses
+
+Support questions relating to IBM Demos Hands-on Labs:
+
+## Possible Questions that might arise: 
+
+Q: What happened to Hands-on Labs on IBM Demos? 
+
+Q: What content will be made available moving forward on IBM Demos?
+
+A: IBM Demos is going through a transfer of ownership, all Hands on labs hosted on Skytap will no longer be available. IBMers and Business Partners are welcome to use assets available on Asset Repo [https://assetrepo.ibm.com/]. For additional inquiries on IBM Demos transfer topics, join #edge-ibmdemos and #edge-ibmdemos-content-support (IBMers only) slack channels to continue discussions there. Please note that the channels are in IBM-Cognitive-Applications cognitive-app.slack.com workspace. If you have problems joining the workspace, please contact Maria Lyashevsky (mlyashev@us.ibm.com).
+
+Q: I needed a hands-on lab that was removed from IBM Demos? 
+
+Response if the questions came from an IBMer:
+Please contact Brooke Jones (brooke.jones@ibm.com) on the DTE team with the name of the hands-on lab and the URL if you have it. If you do not have this information please provide more information on what product the asset was related to, as this will help narrow the search. If content needs to be migrated, keep in mind the following: 
+
+* Content migrated to assetrepo.ibm.com is only IBMer and BP accessible. Customer will not have access. 
+
+* Content must have an owner, if you do not wish to own this content moving forward we will need to work to identify a new owner. Content that does not have an owner that was migrated from IBM Demos will be removed end of Q2. 
+
+* More questions about migrating Hands-on Labs to assetrepo.com can be found here: https://ibm.box.com/s/lewvzg3pabw7kk40eq2rrwi6z4kitytd (contact brooke.jones@ibm.com if there are issues accessing this link)
+
+
 
